5886493fd86700f192eaf0f50916c366852950e3
Alex <agarriga2114@gmail.com>
2022-03-30 14:49:32 -0500

    looptable updates

--- looptable_export.sql
+++ looptable_export.sql
@@ -1,5 +1,5 @@
 #Looping procedure to create a huge table in database_sample to make large CSV
-
+drop table if exists looping_table;
 CREATE TABLE looping_table (
     ID int AUTO_INCREMENT,
     ID2 int,
@@ -11,7 +11,7 @@ CREATE TABLE looping_table (
     ID8 int,
     ID9 int,
     ID10 int,
-    testvarchar varchar(255),
+    testvarchar LONGTEXT,
     testchar char(4),
     testdate date,
     testdecimal DECIMAL(65, 2),
@@ -19,25 +19,28 @@ CREATE TABLE looping_table (
     Primary Key (ID)
 ) AUTO_INCREMENT=1000;
 
-drop procedure InsertLoopingTable;
+drop procedure if exists InsertLoopingTable;
 DELIMITER $$
 
 CREATE PROCEDURE InsertLoopingTable()
 BEGIN
 	DECLARE x INT;
     DECLARE y INT;
+    DECLARE z Longtext;
     SET x = 1;
      SET y = 23;
+     SET z ="test";
      
     insert_loop:  LOOP
-		IF  x > 10000 THEN 
+		IF  x > 1000 THEN 
 			LEAVE  insert_loop;
 		END  IF;
             
 		SET  x = x + 1;
         SET  y = y - 1;
+        SET z =Repeat('test',x);
 INSERT INTO looping_table (ID2,ID3,ID4,ID5,ID6,ID7,ID8,ID9,ID10,testvarchar,testchar,testdate,testdecimal)
-VALUES (x,x*2,x-100,(x*y),y,y+x,y-x,y-23,abs(y),"this is a test","TEST",CURRENT_DATE(),123.23);
+VALUES (x,x*2,x-100,(x*y),y,y+x,y-x,y-23,abs(y),z,"TEST",CURRENT_DATE(),123.23);
 		
 	END LOOP;
 	SELECT count(ID) from looping_table;
@@ -58,10 +61,10 @@ WHERE table_schema = 'database_sample'
 AND table_name = 'looping_table';
 
 #Creating the file
-(Select 'ID','ID2','ID3','ID4','ID5','ID6','ID7','ID8','ID9','ID10','testvarchar','testchar','testdate','testdecimal')
+(Select 'ID','ID2','ID3','ID4','ID5','ID6','ID7','ID8','ID9','ID10','testlongtext','testchar','testdate','testdecimal')
 UNION
 SELECT * from looping_table
-INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/looping_table.csv' 
+INTO OUTFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/looping_table3.csv' 
 FIELDS ENCLOSED BY '"' 
 TERMINATED BY ',' 
 ESCAPED BY '"' 
